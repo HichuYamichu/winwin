c20ff808b5b1b5dde927c916c97b80dd73f1be87
HichuYamichu <hichuyamichu@gmail.com>
2024-07-24 22:46:17 +0200

    --no-edit

--- winwin/src/events.rs
+++ winwin/src/events.rs
@@ -152,6 +152,165 @@ unsafe fn install_pipe_server(
     pool.start_workers_and_accept_connections();
 }
 
+struct IocpWorkerPool {
+    event_tx: SyncSender<(ClientEvent, SyncSender<ServerCommand>)>,
+    io_objects_pool: Receiver<usize>,
+    io_objects_release_channel: Option<SyncSender<usize>>,
+
+    // Alias to allocation holding all `IoData` objects. It is accessed only in drop impl and thus
+    // is safe because drop is guaranteed to be run exclusively by one thread.
+    allocation: usize,
+    pipe_instance_count: usize,
+    iocp: SyncHandle,
+}
+
+impl IocpWorkerPool {
+    fn new(
+        iocp: SyncHandle,
+        event_tx: SyncSender<(ClientEvent, SyncSender<ServerCommand>)>,
+    ) -> Self {
+        let pipe_instance_count = PIPE_INSTANCES_PER_WORKER * THREAD_POOL_SIZE;
+        let (io_objects_tx, io_objects_rx) = sync_channel(pipe_instance_count);
+
+        let (allocation, slice) = unsafe {
+            let layout =
+                alloc::Layout::array::<IoData>(pipe_instance_count).expect("arguments are correct");
+            let mem = alloc::alloc_zeroed(layout);
+            let s = slice::from_raw_parts_mut::<IoData>(mem as *mut _, pipe_instance_count);
+            (mem as usize, s)
+        };
+
+        for slot in slice.iter_mut() {
+            let pipe = unsafe {
+                CreateNamedPipeA(
+                    PIPE_NAME,
+                    PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED,
+                    PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+                    PIPE_UNLIMITED_INSTANCES,
+                    BUFFER_SIZE as u32,
+                    BUFFER_SIZE as u32,
+                    0,
+                    None,
+                )
+            }
+            .unwrap();
+
+            slot.pipe = pipe;
+            let io_data_ptr = slot as *mut _;
+
+            unsafe {
+                CreateIoCompletionPort(pipe, *iocp, io_data_ptr as usize, 0).unwrap();
+            }
+
+            io_objects_tx
+                .send(io_data_ptr as _)
+                .expect("queue is big enough to hold every io_data");
+        }
+
+        Self {
+            event_tx,
+            io_objects_pool: io_objects_rx,
+            io_objects_release_channel: Some(io_objects_tx),
+
+            allocation,
+            iocp,
+            pipe_instance_count,
+        }
+    }
+
+    fn start_workers_and_accept_connections(&mut self) {
+        let io_objects_release_channel = self
+            .io_objects_release_channel
+            .take()
+            .expect("channel was initialized");
+        thread::scope(|s| {
+            for _ in 0..THREAD_POOL_SIZE {
+                let event_tx = self.event_tx.clone();
+                let release_channel = io_objects_release_channel.clone();
+                let iocp = self.iocp;
+                s.spawn(move || unsafe { handle_iocp(iocp, release_channel, event_tx) });
+            }
+
+            // We drop this sender so that only senders left are ones owned by woker threads, this
+            // enables the following loop to exit once all workers quit.
+            drop(io_objects_release_channel);
+
+            while let Some(io_data) = self.io_objects_pool.iter().next() {
+                unsafe {
+                    let io_data = &mut *(io_data as *mut IoData);
+                    IoData::reset(io_data);
+                    let pipe = io_data.pipe;
+                    let overlapped = &mut io_data.overlapped as *mut _;
+                    if let Err(e) = ConnectNamedPipe(pipe, Some(overlapped)) {
+                        if e != ERROR_IO_PENDING.into() {
+                            tracing::warn!(?e);
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+
+impl Drop for IocpWorkerPool {
+    fn drop(&mut self) {
+        tracing::trace!("pool dropped");
+        let slice = unsafe {
+            slice::from_raw_parts_mut::<IoData>(self.allocation as *mut _, self.pipe_instance_count)
+        };
+        for io_data in slice {
+            let _ = unsafe { CloseHandle(io_data.pipe) };
+        }
+
+        let layout = alloc::Layout::array::<IoData>(self.pipe_instance_count)
+            .expect("arguments are correct");
+        unsafe { alloc::dealloc(self.allocation as *mut _, layout) };
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct IoData {
+    overlapped: OVERLAPPED,
+    pipe: HANDLE,
+    buffer: [u8; BUFFER_SIZE],
+    state: State,
+}
+
+impl IoData {
+    fn reset(io_data: *mut Self) {
+        unsafe {
+            let _ = DisconnectNamedPipe((*io_data).pipe);
+            (*io_data).overlapped = OVERLAPPED::default();
+            (*io_data).buffer.as_mut_slice().fill(0);
+            (*io_data).state = State::WaitingForConnection;
+        }
+    }
+
+    unsafe fn as_usize(&mut self) -> usize {
+        self as *mut _ as _
+    }
+}
+
+impl Default for IoData {
+    fn default() -> Self {
+        IoData {
+            overlapped: OVERLAPPED::default(),
+            pipe: INVALID_HANDLE_VALUE,
+            buffer: [0; BUFFER_SIZE],
+            state: State::WaitingForConnection,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum State {
+    WaitingForConnection,
+    ReadEnqueued,
+    WriteEnqueued,
+    WaitingForDisconnect,
+}
+
 unsafe fn handle_iocp(
     iocp: SyncHandle,
     io_objects_release_channel: SyncSender<usize>,
@@ -285,165 +444,6 @@ fn create_io_completion_port() -> SyncHandle {
         CreateIoCompletionPort(INVALID_HANDLE_VALUE, None, 0, THREAD_POOL_SIZE as _).unwrap()
     };
     SyncHandle(iocp)
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-struct IoData {
-    overlapped: OVERLAPPED,
-    pipe: HANDLE,
-    buffer: [u8; BUFFER_SIZE],
-    state: State,
-}
-
-impl IoData {
-    fn reset(io_data: *mut Self) {
-        unsafe {
-            let _ = DisconnectNamedPipe((*io_data).pipe);
-            (*io_data).overlapped = OVERLAPPED::default();
-            (*io_data).buffer.as_mut_slice().fill(0);
-            (*io_data).state = State::WaitingForConnection;
-        }
-    }
-
-    unsafe fn as_usize(&mut self) -> usize {
-        self as *mut _ as _
-    }
-}
-
-impl Default for IoData {
-    fn default() -> Self {
-        IoData {
-            overlapped: OVERLAPPED::default(),
-            pipe: INVALID_HANDLE_VALUE,
-            buffer: [0; BUFFER_SIZE],
-            state: State::WaitingForConnection,
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-enum State {
-    WaitingForConnection,
-    ReadEnqueued,
-    WriteEnqueued,
-    WaitingForDisconnect,
-}
-
-struct IocpWorkerPool {
-    event_tx: SyncSender<(ClientEvent, SyncSender<ServerCommand>)>,
-    io_objects_pool: Receiver<usize>,
-    io_objects_release_channel: Option<SyncSender<usize>>,
-
-    // Alias to allocation holding all `IoData` objects. It is accessed only in drop impl and thus
-    // is safe because drop is guaranteed to be run exclusively by one thread.
-    allocation: usize,
-    pipe_instance_count: usize,
-    iocp: SyncHandle,
-}
-
-impl IocpWorkerPool {
-    fn new(
-        iocp: SyncHandle,
-        event_tx: SyncSender<(ClientEvent, SyncSender<ServerCommand>)>,
-    ) -> Self {
-        let pipe_instance_count = PIPE_INSTANCES_PER_WORKER * THREAD_POOL_SIZE;
-        let (io_objects_tx, io_objects_rx) = sync_channel(pipe_instance_count);
-
-        let (allocation, slice) = unsafe {
-            let layout =
-                alloc::Layout::array::<IoData>(pipe_instance_count).expect("arguments are correct");
-            let mem = alloc::alloc_zeroed(layout);
-            let s = slice::from_raw_parts_mut::<IoData>(mem as *mut _, pipe_instance_count);
-            (mem as usize, s)
-        };
-
-        for slot in slice.iter_mut() {
-            let pipe = unsafe {
-                CreateNamedPipeA(
-                    PIPE_NAME,
-                    PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED,
-                    PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-                    PIPE_UNLIMITED_INSTANCES,
-                    BUFFER_SIZE as u32,
-                    BUFFER_SIZE as u32,
-                    0,
-                    None,
-                )
-            }
-            .unwrap();
-
-            slot.pipe = pipe;
-            let io_data_ptr = slot as *mut _;
-
-            unsafe {
-                CreateIoCompletionPort(pipe, *iocp, io_data_ptr as usize, 0).unwrap();
-            }
-
-            io_objects_tx
-                .send(io_data_ptr as _)
-                .expect("queue is big enough to hold every io_data");
-        }
-
-        Self {
-            event_tx,
-            io_objects_pool: io_objects_rx,
-            io_objects_release_channel: Some(io_objects_tx),
-
-            allocation,
-            iocp,
-            pipe_instance_count,
-        }
-    }
-
-    fn start_workers_and_accept_connections(&mut self) {
-        let io_objects_release_channel = self
-            .io_objects_release_channel
-            .take()
-            .expect("channel was initialized");
-        thread::scope(|s| {
-            for _ in 0..THREAD_POOL_SIZE {
-                let event_tx = self.event_tx.clone();
-                let release_channel = io_objects_release_channel.clone();
-                let iocp = self.iocp;
-                s.spawn(move || unsafe { handle_iocp(iocp, release_channel, event_tx) });
-            }
-
-            // We drop this sender so that only senders left are ones owned by woker threads, this
-            // enables the following loop to exit once all workers quit.
-            drop(io_objects_release_channel);
-
-            while let Some(io_data) = self.io_objects_pool.iter().next() {
-                unsafe {
-                    let io_data = &mut *(io_data as *mut IoData);
-                    IoData::reset(io_data);
-                    let pipe = io_data.pipe;
-                    let overlapped = &mut io_data.overlapped as *mut _;
-                    if let Err(e) = ConnectNamedPipe(pipe, Some(overlapped)) {
-                        if e != ERROR_IO_PENDING.into() {
-                            tracing::warn!(?e);
-                        }
-                    }
-                }
-            }
-        });
-    }
-}
-
-impl Drop for IocpWorkerPool {
-    fn drop(&mut self) {
-        tracing::trace!("pool dropped");
-        let slice = unsafe {
-            slice::from_raw_parts_mut::<IoData>(self.allocation as *mut _, self.pipe_instance_count)
-        };
-        for io_data in slice {
-            let _ = unsafe { CloseHandle(io_data.pipe) };
-        }
-
-        let layout = alloc::Layout::array::<IoData>(self.pipe_instance_count)
-            .expect("arguments are correct");
-        unsafe { alloc::dealloc(self.allocation as *mut _, layout) };
-    }
 }
 
 #[derive(Default)]
